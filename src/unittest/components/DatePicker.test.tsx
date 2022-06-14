4aa255056dd30e0abc0e8118add844fb72ae5d8f
Kuljeet Kaur <kuljeet.kaur@oracle.com>
2022-06-14 22:42:43 +0530

    Correcting typo

--- src/unittest/components/DatePicker.test.tsx
+++ src/unittest/components/DatePicker.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../../redux/store';
 
-test('renders an H3 element', () => {
+test('renders DatePicker component', () => {
     render(<Provider store={store}>
         <DatePicker />
     </Provider>);
--- src/unittest/components/H3.test.tsx
+++ src/unittest/components/H3.test.tsx
@@ -1,9 +1,11 @@
 import {H3} from '../../components/CountHeader/H3';
 import { render, screen } from '@testing-library/react';
 
-test('renders an H3 element', () => {
-    render(<H3 label='Total Count:' value={30} />)
-    const h3Label = screen.getByText(/Total Count:/i);
-    expect(h3Label).toBeInTheDocument();
+test('renders an H3 component that displays total count', () => {
+    render(<H3 label='Total Count:' value={42} />)
+    const h3CountLabel = screen.getByText(/Total Count:/i);
+    expect(h3CountLabel).toBeInTheDocument();
+    const h3CountValue = screen.getByText(/42/);
+    expect(h3CountValue).toBeInTheDocument();
 })
 
